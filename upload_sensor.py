94fea14d415549ecff6c2db48fda8851fe834806
madblocksgit <72651078+madblocksgit@users.noreply.github.com>
2020-12-19 18:18:05 +0530

    Update upload_sensor.py

--- upload_sensor.py
+++ upload_sensor.py
@@ -3,20 +3,18 @@
 # upload_sensor.py - file name
 # sudo nano upload_sensor.py (used to create a python script)
 
-import urllib.request as u # used to invoke API Request of Thingspeak
-import random
+import paho.mqtt.client as mqtt
 import time
 import arduino
 
-write_api='https://api.thingspeak.com/update?api_key=5STFKDRBBZF3JYQ0&field1='
-
 # upload the data to cloud
-def upload_data_to_thingspeak(moisture_value):
-	print('Uploading Data to Thingspeak')
+def upload_data_to_colab(moisture_value):
+	print('Uploading Data to colab')
 	print('Soil Moisture Value: ',moisture_value)
-	a=u.urlopen(write_api+str(moisture_value))
-	print(a)
-	print('Data Uploaded to Thingspeak')
+	client=mqtt.Client()
+	client.connect('broker.hivemq.com',1883)
+	client.publish('nitw/iot',moisture_value)
+	print('Data Uploaded to Colab')
 
 # read the data from sensor
 def read_from_esp(): # static value
@@ -28,6 +26,6 @@ def read_from_esp(): # static value
 # main logic
 while -5: # True
 	m=read_from_esp()
-	upload_data_to_thingspeak(m)
+	upload_data_to_colab(m)
 	time.sleep(5)
 
